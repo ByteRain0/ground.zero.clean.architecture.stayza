414c5d10795bb3a8507eafeb8de9c61d4cd1397d
ddicusar <dicusar.dv@gmail.com>
2025-09-15 11:50:24 +0300

    test: add CancelReservationShould test

--- tests/Stayza.Tests.Unit/Loans/Tests/CancelReservationShould.cs
+++ tests/Stayza.Tests.Unit/Loans/Tests/CancelReservationShould.cs
@@ -1,4 +1,5 @@
 using Shouldly;
+using Stayza.Core.Exceptions;
 using Stayza.Domain.Loans;
 using Stayza.Domain.Loans.Events;
 using Stayza.Tests.Unit.Utils;
@@ -11,24 +12,83 @@ public class CancelReservationShould
     [Fact]
     public void Fail_if_reservation_does_not_exist()
     {
-        // HW
+        // Arrange
+        var bookCopy = TestBooksFactory.CreateBookCopy();
+
+        // Act 
+        var action = () => bookCopy.CancelReservation(
+            reservationId: Guid.NewGuid(),
+            "cancel");
+
+        // Assert
+        var ex = Should.Throw<EntityNotFoundException>(action);
+        ex.EntityType.ShouldBe(nameof(Reservation));
     }
 
     [Fact]
     public void Fail_if_reservation_already_cancelled()
     {
-        // HW
+        // Arrange
+        var bookCopy = TestBooksFactory.CreateBookCopy();
+
+        var reservation = bookCopy.Reserve(
+            userId: Constants.Users.Id,
+            utcNow: Constants.Time.TestUtcNow);
+        
+        var action = () => bookCopy.CancelReservation(
+            reservationId: reservation.Id,
+            "cancel");
+
+        action.Invoke();
+
+        // Assert & Act
+        var ex = Should.Throw<EntityNotFoundException>(action);
+        ex.EntityType.ShouldBe(nameof(Reservation));
     }
 
     [Fact]
     public void Cancel_valid_reservation()
     {
-        // HW
+        // Arrange
+        var bookCopy = TestBooksFactory.CreateBookCopy();
+
+        var reservation = bookCopy.Reserve(
+            userId: Constants.Users.Id,
+            utcNow: Constants.Time.TestUtcNow);
+        
+        // Act
+        bookCopy.CancelReservation(
+            reservationId: reservation.Id,
+            "cancel");
+
+        // Assert
+        reservation.Status.ShouldBe(ReservationStatus.Cancelled);
     }
 
     [Fact]
     public void Publish_ReservationCancelledEvent()
     {
-        // HW
+        // Arrange
+        var bookCopy = TestBooksFactory.CreateBookCopy();
+        const string cancelReason = "cancel";
+
+        var reservation = bookCopy.Reserve(
+            userId: Constants.Users.Id,
+            utcNow: Constants.Time.TestUtcNow);
+        
+        // Act
+        bookCopy.CancelReservation(
+            reservationId: reservation.Id,
+            reason: cancelReason);
+
+        // Assert
+        bookCopy.DomainEvents.OfType<ReservationCancelledEvent>().Count().ShouldBe(1);
+
+        var @event = bookCopy.DomainEvents.OfType<ReservationCancelledEvent>().Single();
+        
+        @event.ReservationId.ShouldBe(reservation.Id);
+        @event.UserId.ShouldBe(reservation.UserId);
+        @event.BookCopyId.ShouldBe(bookCopy.Id);
+        @event.Reason.ShouldBe(cancelReason);
     }
 }
