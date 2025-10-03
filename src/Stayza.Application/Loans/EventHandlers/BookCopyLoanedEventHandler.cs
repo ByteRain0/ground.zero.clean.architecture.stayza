f4a0aef85229f363db95acdb321d883e7254c8d0
vasileoleinic <vasilii.oleinic@indrivo.com>
2025-10-03 19:38:36 +0300

    In class work.

--- src/Stayza.Application/Loans/EventHandlers/BookCopyLoanedEventHandler.cs
+++ src/Stayza.Application/Loans/EventHandlers/BookCopyLoanedEventHandler.cs
@@ -1,9 +1,15 @@
+using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Stayza.Core.Messaging;
 using Stayza.Core.Telemetry;
+using Stayza.Domain.Loans;
+using Stayza.Domain.Loans.Events;
 
 namespace Stayza.Application.Loans.EventHandlers;
 
-public class BookCopyLoanedEventHandler : IListener
+public class BookCopyLoanedEventHandler
+    (IServiceScopeFactory serviceScopeFactory): IListener
 {
     public string RoutingKey => RoutingKeys.BookCopyLoanedTopic
         .ReplaceBookCopyIdPlaceholderWith("*");
@@ -11,7 +17,22 @@ public class BookCopyLoanedEventHandler : IListener
     public Task ProcessMessage(Message message, string routingKey)
     {
         using var activity = RunTimeDiagnosticConfig.Source.StartActivity("Handling book loaned event");
-        Console.WriteLine("You might want to do something here :) ");
+        
+        using var scope = serviceScopeFactory.CreateScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<ReservationFulfilledEventHandler>>();
+        var notificationService = scope.ServiceProvider.GetRequiredService<IUserNotificationService>();
+        
+        var incomingEvent = JsonSerializer.Deserialize<BookLoanedEvent>(message.Body);
+
+        if (incomingEvent is null)
+        {
+            logger.LogCritical(
+                "Issues encountered processing events of type : {eventType}.",
+                nameof(ReservationFulfilledEvent));
+        }
+
+        notificationService.NotifyUser(incomingEvent.UserId, nameof(BookLoanedEvent));
+        
         return Task.CompletedTask;
     }
 }
